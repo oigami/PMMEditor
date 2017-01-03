fe1e117e4bde100515a3b26118ac638bc13aaa01
oigami <oigami013@yahoo.co.jp>
2017-01-04 01:34:28 +0900

    ビューの追加

--- PMMEditor/ViewModels/MainWindowViewModel.cs
+++ PMMEditor/ViewModels/MainWindowViewModel.cs
@@ -1,36 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.ComponentModel;
-
+﻿using System.IO;
 using Livet;
-using Livet.Commands;
-using Livet.Messaging;
-using Livet.Messaging.IO;
-using Livet.EventListeners;
-using Livet.Messaging.Windows;
-
 using PMMEditor.Models;
 
 namespace PMMEditor.ViewModels
 {
     public class MainWindowViewModel : ViewModel
     {
-        /* コマンド、プロパティの定義にはそれぞれ 
-         * 
+        /* コマンド、プロパティの定義にはそれぞれ
+         *
          *  lvcom   : ViewModelCommand
          *  lvcomn  : ViewModelCommand(CanExecute無)
          *  llcom   : ListenerCommand(パラメータ有のコマンド)
          *  llcomn  : ListenerCommand(パラメータ有のコマンド・CanExecute無)
          *  lprop   : 変更通知プロパティ(.NET4.5ではlpropn)
-         *  
+         *
          * を使用してください。
-         * 
+         *
          * Modelが十分にリッチであるならコマンドにこだわる必要はありません。
          * View側のコードビハインドを使用しないMVVMパターンの実装を行う場合でも、ViewModelにメソッドを定義し、
          * LivetCallMethodActionなどから直接メソッドを呼び出してください。
-         * 
+         *
          * ViewModelのコマンドを呼び出せるLivetのすべてのビヘイビア・トリガー・アクションは
          * 同様に直接ViewModelのメソッドを呼び出し可能です。
          */
@@ -42,25 +31,75 @@ namespace PMMEditor.ViewModels
         /* Modelからの変更通知などの各種イベントを受け取る場合は、PropertyChangedEventListenerや
          * CollectionChangedEventListenerを使うと便利です。各種ListenerはViewModelに定義されている
          * CompositeDisposableプロパティ(LivetCompositeDisposable型)に格納しておく事でイベント解放を容易に行えます。
-         * 
+         *
          * ReactiveExtensionsなどを併用する場合は、ReactiveExtensionsのCompositeDisposableを
          * ViewModelのCompositeDisposableプロパティに格納しておくのを推奨します。
-         * 
+         *
          * LivetのWindowテンプレートではViewのウィンドウが閉じる際にDataContextDisposeActionが動作するようになっており、
          * ViewModelのDisposeが呼ばれCompositeDisposableプロパティに格納されたすべてのIDisposable型のインスタンスが解放されます。
-         * 
+         *
          * ViewModelを使いまわしたい時などは、ViewからDataContextDisposeActionを取り除くか、発動のタイミングをずらす事で対応可能です。
          */
 
         /* UIDispatcherを操作する場合は、DispatcherHelperのメソッドを操作してください。
          * UIDispatcher自体はApp.xaml.csでインスタンスを確保してあります。
-         * 
+         *
          * LivetのViewModelではプロパティ変更通知(RaisePropertyChanged)やDispatcherCollectionを使ったコレクション変更通知は
          * 自動的にUIDispatcher上での通知に変換されます。変更通知に際してUIDispatcherを操作する必要はありません。
          */
+        private Model _model;
+
 
         public void Initialize()
         {
+            _model = new Model();
+            PmmStruct =
+                new PmmReader(File.ReadAllBytes("C:/tool/MikuMikuDance_v926x64/UserFile/サンプル（きしめんAllStar).pmm")).Read();
+            RaisePropertyChanged(nameof(ModelList));
         }
+
+        #region ModelTab
+
+        #region ModelList変更通知プロパティ
+
+        public PmmStuct.ModelData[] ModelList => PmmStruct.ModelDatas;
+
+        #endregion ModelList変更通知プロパティ
+
+        #region SelectedModel変更通知プロパティ
+
+        private PmmStuct.ModelData _SelectedModel;
+
+        public PmmStuct.ModelData SelectedModel
+        {
+            get { return _SelectedModel; }
+
+            set
+            {
+                _SelectedModel = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        #endregion SelectedModel変更通知プロパティ
+
+        #endregion ModelTab
+
+        #region PmmStruct変更通知プロパティ
+
+        private PmmStuct _PmmStruct;
+
+        public PmmStuct PmmStruct
+        {
+            get { return _PmmStruct; }
+
+            set
+            {
+                _PmmStruct = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        #endregion PmmStruct変更通知プロパティ
     }
 }
