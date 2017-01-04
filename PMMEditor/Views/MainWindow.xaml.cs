83504243d47900d2f3f0f331f2aeb01c3593f907
oigami <oigami013@yahoo.co.jp>
2017-01-04 20:59:29 +0900

    ビューの更新

--- PMMEditor/Views/MainWindow.xaml.cs
+++ PMMEditor/Views/MainWindow.xaml.cs
@@ -1,21 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Globalization;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PMMEditor.Views
 {
-    /* 
-	 * ViewModelからの変更通知などの各種イベントを受け取る場合は、PropertyChangedWeakEventListenerや
+    public class ByteArrayToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var bytes = value as byte[];
+            if (bytes == null)
+            {
+                throw new ArgumentException("value is not byte[]", nameof(value));
+            }
+            return string.Join(",", Array.ConvertAll(bytes, o => $"{o,3}"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /*
+     * ViewModelからの変更通知などの各種イベントを受け取る場合は、PropertyChangedWeakEventListenerや
      * CollectionChangedWeakEventListenerを使うと便利です。独自イベントの場合はLivetWeakEventListenerが使用できます。
      * クローズ時などに、LivetCompositeDisposableに格納した各種イベントリスナをDisposeする事でイベントハンドラの開放が容易に行えます。
      *
