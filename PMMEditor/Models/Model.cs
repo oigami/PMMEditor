3a3b3ee0cc4f8a9450c7cd5b6d7befa9da7baabb
oigami <oigami013@yahoo.co.jp>
2017-01-26 10:11:50 +0900

    圧縮方法をzipに変更

--- PMMEditor/Models/Model.cs
+++ PMMEditor/Models/Model.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
@@ -24,15 +25,26 @@ namespace PMMEditor.Models
 
         public async Task SavePmmJson(string filename, bool isCompress = false)
         {
-            var json = JsonConvert.SerializeObject(PmmStruct);
+            var json = JsonConvert.SerializeObject(PmmStruct, new JsonSerializerSettings
+            {
+                Culture = new CultureInfo("ja-JP"),
+                DateFormatHandling = DateFormatHandling.MicrosoftDateFormat,
+                DateTimeZoneHandling = DateTimeZoneHandling.Local,
+                Formatting = Formatting.None,
+                StringEscapeHandling = StringEscapeHandling.EscapeNonAscii
+            });
             if (isCompress)
             {
-                using (var fso = new FileStream(filename, FileMode.CreateNew))
+                using (var fso = new FileStream(filename, FileMode.Create))
                 {
-                    using (var ds = new DeflateStream(fso, CompressionMode.Compress))
+                    using (var ds = new ZipArchive(fso, ZipArchiveMode.Create))
                     {
-                        var data = Encoding.ASCII.GetBytes(json);
-                        await ds.WriteAsync(data, 0, data.Length);
+                        var entry = ds.CreateEntry("pmm.json", CompressionLevel.Optimal);
+                        using (var stream = entry.Open())
+                        {
+                            var data = Encoding.GetEncoding("Shift_JIS").GetBytes(json);
+                            await stream.WriteAsync(data, 0, data.Length);
+                        }
                     }
                 }
             }
