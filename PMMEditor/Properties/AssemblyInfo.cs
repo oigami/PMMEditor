6c6f843c3fe4e033d903299081fc3faea3294d50
oigami <oigami013@yahoo.co.jp>
2017-01-29 04:21:14 +0900

    ファイルのフォーマット

--- PMMEditor/Properties/AssemblyInfo.cs
+++ PMMEditor/Properties/AssemblyInfo.cs
@@ -7,6 +7,7 @@ using System.Windows;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
+
 [assembly: AssemblyTitle("PMMEditor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -19,6 +20,7 @@ using System.Windows;
 // ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから
 // 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
 // その型の ComVisible 属性を true に設定してください。
+
 [assembly: ComVisible(false)]
 
 //ローカライズ可能なアプリケーションのビルドを開始するには、
@@ -33,12 +35,12 @@ using System.Windows;
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //テーマ固有のリソース ディクショナリが置かれている場所
-                                     //(リソースがページ、
-                                     //またはアプリケーション リソース ディクショナリに見つからない場合に使用されます)
+    //(リソースがページ、
+    //またはアプリケーション リソース ディクショナリに見つからない場合に使用されます)
     ResourceDictionaryLocation.SourceAssembly //汎用リソース ディクショナリが置かれている場所
-                                              //(リソースがページ、
-                                              //アプリケーション、またはいずれのテーマ固有のリソース ディクショナリにも見つからない場合に使用されます)
-)]
+    //(リソースがページ、
+    //アプリケーション、またはいずれのテーマ固有のリソース ディクショナリにも見つからない場合に使用されます)
+    )]
 
 
 // アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
@@ -51,5 +53,6 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
