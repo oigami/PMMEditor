ba7b7eedd461132132788830c255222ac9425056
oigami <oigami013@yahoo.co.jp>
2017-02-02 19:27:33 +0900

    タイムラインの名前リストをツリービューに変更

--- PMMEditor/ViewModels/Documents/CameraLightAccessoryViewModel.cs
+++ PMMEditor/ViewModels/Documents/CameraLightAccessoryViewModel.cs
@@ -4,21 +4,18 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.ComponentModel;
-
 using Livet;
 using Livet.Commands;
 using Livet.Messaging;
 using Livet.Messaging.IO;
 using Livet.EventListeners;
 using Livet.Messaging.Windows;
-
 using PMMEditor.Models;
 
 namespace PMMEditor.ViewModels.Documents
 {
     public class CameraLightAccessoryViewModel : TimelineViewModelBase
     {
-
         public CameraLightAccessoryViewModel(Model model) : base(model)
         {
             CreateKeyFrame();
@@ -26,35 +23,35 @@ namespace PMMEditor.ViewModels.Documents
 
         private void CreateKeyFrame()
         {
-            ListOfKeyFrameList = new ObservableCollection<KeyFrameList>
+            ListOfKeyFrameList = new ObservableCollection<TimelineKeyFrameList>
+            {
+                new TimelineKeyFrameList
                 {
-                    new KeyFrameList
-                    {
-                        Name = "Camera",
-                        Frame = CreateList(_model.PmmStruct.CameraKeyFrames, _model.PmmStruct.CameraInitFrame)
-                    },
-                    new KeyFrameList
-                    {
-                        Name = "Light",
-                        Frame = CreateList(_model.PmmStruct.LightKeyFrames, _model.PmmStruct.LightInitFrame)
-                    },
-                    new KeyFrameList
-                    {
-                        Name = "Self Shadow",
-                        Frame = CreateList(_model.PmmStruct.SelfShadowKeyFrames, _model.PmmStruct.SelfShadowInitFrame)
-                    },
-                    new KeyFrameList
-                    {
-                        Name = "Gravity",
-                        Frame = CreateList(_model.PmmStruct.GravityKeyFrames, _model.PmmStruct.GravityInitFrame)
-                    }
-                };
+                    Name = "Camera",
+                    Frame = CreateList(_model.PmmStruct.CameraKeyFrames, _model.PmmStruct.CameraInitFrame)
+                },
+                new TimelineKeyFrameList
+                {
+                    Name = "Light",
+                    Frame = CreateList(_model.PmmStruct.LightKeyFrames, _model.PmmStruct.LightInitFrame)
+                },
+                new TimelineKeyFrameList
+                {
+                    Name = "Self Shadow",
+                    Frame = CreateList(_model.PmmStruct.SelfShadowKeyFrames, _model.PmmStruct.SelfShadowInitFrame)
+                },
+                new TimelineKeyFrameList
+                {
+                    Name = "Gravity",
+                    Frame = CreateList(_model.PmmStruct.GravityKeyFrames, _model.PmmStruct.GravityInitFrame)
+                }
+            };
 
 
             var accessoryData = _model.PmmStruct.AccessoryDatas;
             foreach (PmmStruct.AccessoryData item in accessoryData)
             {
-                ListOfKeyFrameList.Add(new KeyFrameList
+                ListOfKeyFrameList.Add(new TimelineKeyFrameList
                 {
                     Name = item.Name,
                     Frame = CreateList(item.KeyFrames, item.InitFrame)
--- PMMEditor/ViewModels/Documents/TimelineViewModelBase.cs
+++ PMMEditor/ViewModels/Documents/TimelineViewModelBase.cs
@@ -9,23 +9,58 @@ using PMMEditor.Models;
 
 namespace PMMEditor.ViewModels.Documents
 {
+    public struct TimelineFrameData
+    {
+        public int FrameNumber { get; set; }
+    }
+
+    public class TimelineKeyFrameList : ViewModel
+    {
+        #region IsExpanded変更通知プロパティ
+
+        private bool _IsExpanded;
+
+        public bool IsExpanded
+        {
+            get { return _IsExpanded; }
+            set
+            {
+                if (_IsExpanded == value)
+                {
+                    return;
+                }
+                _IsExpanded = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        #endregion
+
+        public List<TimelineFrameData> Frame { get; set; }
+
+        public string Name { get; set; }
+
+        public List<TimelineKeyFrameList> Children { get; set; }
+    }
+
     public abstract class TimelineViewModelBase : DocumentViewModelBase
     {
         protected readonly Model _model;
 
-        protected static List<FrameData> CreateList<T>(List<T> frameList, T beginFrame) where T : PmmStruct.IKeyFrame
+        protected static List<TimelineFrameData> CreateList<T>(List<T> frameList, T beginFrame)
+            where T : PmmStruct.IKeyFrame
         {
             if (beginFrame == null)
             {
                 return null;
             }
-            var res = new List<FrameData>();
+            var res = new List<TimelineFrameData>();
             var item = beginFrame;
-            res.Add(new FrameData {FrameNumber = item.FrameNumber});
+            res.Add(new TimelineFrameData {FrameNumber = item.FrameNumber});
             int nowIndex = item.NextIndex;
             while (nowIndex != 0)
             {
-                res.Add(new FrameData {FrameNumber = item.FrameNumber});
+                res.Add(new TimelineFrameData {FrameNumber = item.FrameNumber});
                 item = frameList.FirstOrDefault(frame => frame.DataIndex == nowIndex);
                 if (item == null)
                 {
@@ -41,23 +76,11 @@ namespace PMMEditor.ViewModels.Documents
             _model = model;
         }
 
-        public struct FrameData
-        {
-            public int FrameNumber { get; set; }
-        }
-
-        public struct KeyFrameList
-        {
-            public List<FrameData> Frame { get; set; }
-
-            public string Name { get; set; }
-        }
-
         #region ListOfKeyFrameList変更通知プロパティ
 
-        private ObservableCollection<KeyFrameList> _ListOfKeyFrameList;
+        private ObservableCollection<TimelineKeyFrameList> _ListOfKeyFrameList;
 
-        public ObservableCollection<KeyFrameList> ListOfKeyFrameList
+        public ObservableCollection<TimelineKeyFrameList> ListOfKeyFrameList
         {
             get { return _ListOfKeyFrameList; }
             set
