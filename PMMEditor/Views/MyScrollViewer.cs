44424ba91a262fc9273628282a39a12bb4950276
oigami <oigami013@yahoo.co.jp>
2017-02-13 23:59:47 +0900

    ScrollViewer周りのクラスを追加

---
+++ PMMEditor/Views/MyScrollViewer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PMMEditor.Views
+{
+    /*
+     * ScrollViewerのイベントを下層のコントロールへ透過させる方法
+     * http://qiita.com/takanemu/items/a82c8f4d68c7c31e7138
+     */
+
+    internal class MyScrollViewer : ScrollViewer
+    {
+        protected override HitTestResult HitTestCore(PointHitTestParameters hitTestParameters)
+        {
+            return null;
+        }
+    }
+}
