4c1f799f556cb11cab0946cd504e12f78edbc37e
oigami <oigami013@yahoo.co.jp>
2017-04-02 15:25:21 +0900

    カメラ位置のリセット機能を追加

--- PMMEditor/ViewModels/CameraControlViewModel.cs
+++ PMMEditor/ViewModels/CameraControlViewModel.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
+using Livet.Commands;
 using PMMEditor.Models;
 using PMMEditor.MVVM;
 using Reactive.Bindings;
@@ -11,6 +15,41 @@ using SharpDX;
 
 namespace PMMEditor.ViewModels
 {
+    public struct CameraData
+    {
+        [TypeConverter(typeof(StringToVector3Array))]
+        public Vector3 LookAt { get; set; }
+
+        [TypeConverter(typeof(StringToVector3Array))]
+        public Vector3 Rotate { get; set; }
+
+        public float Distance { get; set; }
+    }
+
+    public class StringToVector3Array : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (sourceType == typeof(string))
+            {
+                return true;
+            }
+            return base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            var str = (string) value;
+            var s = str.Split(',');
+            var arr = new float[s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                float.TryParse(s[i], out arr[i]);
+            }
+            return new Vector3(arr[0], arr[1], arr[2]);
+        }
+    }
+
     public class CameraControlViewModel : BindableDisposableBase
     {
         public CameraControlModel Model { get; }
@@ -92,8 +131,14 @@ namespace PMMEditor.ViewModels
             set { SetRotate(2, value); }
         }
 
+        #region ResetFrontCommand
 
+        private ListenerCommand<CameraData> _resetCameraCommand;
+
+        public ListenerCommand<CameraData> ResetCameraCommand
+            => _resetCameraCommand ?? (_resetCameraCommand = new ListenerCommand<CameraData>(
+                _ => Model.SetView(_.LookAt, Models.MathUtil.DegreesToRadians(_.Rotate), _.Distance)));
+
+        #endregion
     }
-
-
 }
